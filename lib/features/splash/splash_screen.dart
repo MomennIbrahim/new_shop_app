d681b114d57af6fb2cbb1b13d3736156e888f62c
MomennIbrahim <ibrahimmomen179@gmail.com>
2023-11-07 15:32:01 +0200

    updated splash screen

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,42 +1,25 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:route_app/core/constance.dart';
-import 'package:route_app/core/widgets/custom_navigator.dart';
-import 'package:route_app/features/auth/presentation/screens/sign_in_screen.dart';
-import 'package:route_app/features/layout/presentation/screens/layout_screen.dart';
+import 'package:route_app/core/utils/styles.dart';
+import '../../core/constance.dart';
+import '../auth/presentation/screens/sign_in_screen.dart';
+import '../layout/presentation/screens/layout_screen.dart';
 
-import '../../core/utils/local_storage.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-        Future.delayed(
-      const Duration(seconds: 3),
-      () => customNavigatorAndReplacement(
-          context: context,
-          widget: token != null ? const LayoutScreen() : SignInScreen()),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: BoxDecoration(
-          color: kPrimaryColor,
-          image: const DecorationImage(
-              image: AssetImage('assets/images/Splash Screen.png'),
-              fit: BoxFit.cover),
+      body: Center(
+        child: AnimatedSplashScreen(
+          splash: Text('Online Store',style: Styles.style24,),
+          nextScreen:  token != null ? const LayoutScreen() : SignInScreen(),
+          duration: 2500,
+          backgroundColor: kPrimaryColor,
+          splashTransition: SplashTransition.fadeTransition,
+          animationDuration: const Duration(seconds: 1),
         ),
       ),
     );
